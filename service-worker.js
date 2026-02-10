b98f09d663164ccad1b5a11afb817b8b4f490a11
ismaelsantanaeletro-design <ismaelsantana.eletro@gmail.com>
2026-02-10 11:13:32 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,23 @@
-const CACHE_NAME = "reforco-v2";
+const CACHE_NAME = "reforco-escolar-v1";
 
-const FILES = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+const urlsToCache = [
+  "/reforco-escolar/",
+  "/reforco-escolar/index.html",
+  "/reforco-escolar/manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
